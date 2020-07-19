246ab925b157be53268c206278314484e6b8e4e7
Nguyen Viet Huan <ngnviethuan@gmail.com>
2020-07-20 00:41:12 +0700

    update contents

--- assets/javascripts/about.js
+++ assets/javascripts/about.js
@@ -141,40 +141,40 @@ document.addEventListener('DOMContentLoaded', function (event) {
     },
   ];
   
-  const skillWrapper = window.document.getElementById('skill-list');
+  // const skillWrapper = window.document.getElementById('skill-list');
   
-  skillDatas.forEach(skill => {
-    const h4 = addElement(skillWrapper, document.createElement('h4'), {
-      id: skill.name.replace(/\s/g, ''),
-      style: 'font-size: 14px;'
-    });
-    addElement(h4, document.createElement('a'), {
-      href: `#${ skill.name.replace(/\s/g, '') }`,
-      style: 'color: #438ab3; display: inline;'
-    }).innerHTML = `# ${ skill.name }:`;
-    addElement(h4, document.createElement('span'), {
-      style: 'color: #aaa; font-weight: 100; font-style: italic;'
-    }).innerHTML = skill.Quotes && `&nbsp;"${ skill.Quotes }"`;
+  // skillDatas.forEach(skill => {
+  //   const h4 = addElement(skillWrapper, document.createElement('h4'), {
+  //     id: skill.name.replace(/\s/g, ''),
+  //     style: 'font-size: 14px;'
+  //   });
+  //   addElement(h4, document.createElement('a'), {
+  //     href: `#${ skill.name.replace(/\s/g, '') }`,
+  //     style: 'color: #438ab3; display: inline;'
+  //   }).innerHTML = `# ${ skill.name }:`;
+  //   addElement(h4, document.createElement('span'), {
+  //     style: 'color: #aaa; font-weight: 100; font-style: italic;'
+  //   }).innerHTML = skill.Quotes && `&nbsp;"${ skill.Quotes }"`;
 
-    const ulSkill = addElement(skillWrapper, document.createElement('ul'), {
-      style: 'list-style-type: none',
-    });
-    Object.getOwnPropertyNames(skill).forEach(key => {
-      if(key !== 'name' && Array.isArray(skill[key]) && skill[key].length) {
-        const li = addElement(ulSkill, document.createElement('li'));
-        addElement(li, document.createElement('strong')).innerHTML = `${ key }: `;
-        skill[key].forEach((e, i) => {
-          addElement(li, document.createElement('a'), {
-            href: e.ref,
-            target: /\#.*/g.test(e.ref) ? '' : '_blank',
-            style: `
-              text-decoration: underline;
-              font-style: italic;
-            `
-          }).innerHTML = `${ e.name }`;
-          addElement(li, document.createElement('span')).innerHTML = `${ i === skill[key].length - 1 ? '' : ', ' }`;
-        });
-      }
-    })
-  });
+  //   const ulSkill = addElement(skillWrapper, document.createElement('ul'), {
+  //     style: 'list-style-type: none',
+  //   });
+  //   Object.getOwnPropertyNames(skill).forEach(key => {
+  //     if(key !== 'name' && Array.isArray(skill[key]) && skill[key].length) {
+  //       const li = addElement(ulSkill, document.createElement('li'));
+  //       addElement(li, document.createElement('strong')).innerHTML = `${ key }: `;
+  //       skill[key].forEach((e, i) => {
+  //         addElement(li, document.createElement('a'), {
+  //           href: e.ref,
+  //           target: /\#.*/g.test(e.ref) ? '' : '_blank',
+  //           style: `
+  //             text-decoration: underline;
+  //             font-style: italic;
+  //           `
+  //         }).innerHTML = `${ e.name }`;
+  //         addElement(li, document.createElement('span')).innerHTML = `${ i === skill[key].length - 1 ? '' : ', ' }`;
+  //       });
+  //     }
+  //   })
+  // });
 });
